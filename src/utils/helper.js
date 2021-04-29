963689f9117e40a1c3b3b1268e139a5b1a4473e0
EsraaNasr92 <esraanasr706@gmail.com>
2021-04-29 15:21:09 +0200

    add api and helpers file

---
+++ src/utils/helper.js
@@ -0,0 +1,21 @@
+export function formatDate (timestamp) {
+  const d = new Date(timestamp)
+  const time = d.toLocaleTimeString('en-US')
+  return time.substr(0, 5) + time.slice(-2) + ' | ' + d.toLocaleDateString()
+}
+
+export function formatQuestion (question, author, authedUser) {
+
+  const { id, optionOne, optionTwo, timestamp } = question
+  const { name, avatarURL } = author
+
+  return {
+    name,
+    id,
+    timestamp,
+    avatar: avatarURL,
+    optionOne: optionOne.votes.length,
+    optionTwo: optionTwo.votes.length,
+  }
+
+}

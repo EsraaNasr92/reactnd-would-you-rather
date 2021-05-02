c6bc37ec2fcb5d2891c181d7f6d46a7261177a0c
EsraaNasr92 <esraanasr706@gmail.com>
2021-05-02 20:41:23 +0200

    not found

---
+++ src/components/Notfound.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class Notfound extends Component {
+  render() {
+    return(
+      <div class="dashboard">
+        <h3 className='page-header'>404 page not found</h3>
+      </div>
+    )
+  }
+}
+export default Notfound
